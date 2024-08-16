d9e2647b39b425877eda9243c841e3494a81a827
Chyngyz <konuratov.chika@gmail.com>
2024-08-16 19:17:58 +0600

    Fixed script

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-python3 -m venv venv
-. venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
