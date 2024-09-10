3c808c6682de4461f87eeac0ab207b11ceb14e74
Chyngyz <konuratov.chika@gmail.com>
2024-09-10 20:26:16 +0600

    Fixed cors trusted origins

--- config/settings/base.py
+++ config/settings/base.py
@@ -280,10 +280,13 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000',
-#     'http://localhost:5173',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    "http://tilimpoz1.vercel.app",
+    '127.0.0.1:3000',
+    '127.0.0.1:5173',
+]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000/',
