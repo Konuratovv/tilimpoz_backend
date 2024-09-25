1aafa13c22b8a4e02a47a8ee10f7d6a6606c56d5
Chyngyz <konuratov.chika@gmail.com>
2024-09-25 17:13:44 +0600

    made filter by id

--- apps/books/filters.py
+++ apps/books/filters.py
@@ -2,14 +2,14 @@ from django_filters import rest_framework as filters
 from .models import Book
 
 class BookFilter(filters.FilterSet):
-    book_category = filters.CharFilter(method='filter_book_category')
+    book_category_id = filters.CharFilter(method='filter_book_category')
 
 
     class Meta:
         model = Book
-        fields = ('book_category', )
+        fields = ('book_category_id', )
         
     def filter_book_category(self, queryset, name, value):
-        if value is None or value.lower() == 'бардыгы':
+        if value is None or value == '0':
             return queryset
-        return queryset.filter(book_category__title__icontains=value)
+        return queryset.filter(book_category__id=value)
--- apps/quiz/filters.py
+++ apps/quiz/filters.py
@@ -2,14 +2,14 @@ from django_filters import rest_framework as filters
 from .models import Test
 
 class TestFilter(filters.FilterSet):
-    category__title = filters.CharFilter(method='filter_category_title')
+    category__id = filters.CharFilter(method='filter_category_id')
 
 
     class Meta:
         model = Test
-        fields = ('category__title', )
+        fields = ('category__id', )
         
-    def filter_category_title(self, queryset, name, value):
-        if not value or value.lower() == 'бардыгы':
+    def filter_category_id(self, queryset, name, value):
+        if not value or value == '0':
             return queryset
-        return queryset.filter(category__title__icontains=value)
+        return queryset.filter(category__id=value)
